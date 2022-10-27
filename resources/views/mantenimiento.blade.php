eb266a98ad3698cf587c52105221119b5857ddd3
AntonioCarlosISC <antoniocarlosrd.isc@gmail.com>
2022-10-26 19:19:52 -0500

    Edición del proyecto y ramas. Se agregarón nuevas vistas

---
+++ resources/views/mantenimiento.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Estado de mantenimiento</h1>
+@stop
+
+@section('content')
+    <p>(Aquí se mostrará el inventario y su estado de mantenimiento)</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ resources/views/tecnico.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Servicio Técnico</h1>
+@stop
+
+@section('content')
+    <p>(Se mostrarán las notificaciones y mantenimiento en espera)</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
